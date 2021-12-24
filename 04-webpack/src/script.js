3d148157f44094631693dc59bedc76438692f48a
Hyeon1445 <lily1445@gmail.com>
2021-12-24 14:24:11 +0900

    04. webpack

---
+++ 04-webpack/src/script.js
@@ -0,0 +1,28 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Scene
+const scene = new THREE.Scene()
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Camera
+const sizes = {
+  width: 800,
+  height: 600
+}
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(75, aspectRatio)
+camera.position.z = 4
+camera.position.x = 1
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('canvas.webgl')
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
