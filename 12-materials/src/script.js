0261b993a2b23196caad59b3c8a1a665867247a0
Hyeon1445 <lily1445@gmail.com>
2022-01-30 22:38:11 +0900

    12-5. Materials (MeshLambertMaterial)

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -56,9 +56,23 @@ const scene = new THREE.Scene()
 // material.wireframe = true
 // material.flatShading = true
 
-const material = new THREE.MeshMatcapMaterial()
-material.matcap = matcapTexture
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
 
+// const material = new THREE.MeshDepthMaterial()
+const material = new THREE.MeshLambertMaterial()
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(0.5, 16, 16),
