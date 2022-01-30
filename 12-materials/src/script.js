55fcb931c2bc48823e755de74d7fe9458d8ce4ae
Hyeon1445 <lily1445@gmail.com>
2022-01-30 23:17:23 +0900

    12-8. Materials (MeshStandardMaterial)

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -1,6 +1,13 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+
 
 /**
  * Textures
@@ -67,13 +74,34 @@ const scene = new THREE.Scene()
 // material.shininess = 100
 // material.specular = new THREE.Color(0xff0000)
 
-const material = new THREE.MeshToonMaterial()
-material.gradientMap = gradientTexture
+// const material = new THREE.MeshToonMaterial()
+// material.gradientMap = gradientTexture
+const material = new THREE.MeshStandardMaterial()
+// material.metalness = 0.45
+// material.roughness = 0.65
+material.map = doorColorTexture
+material.aoMap = doorAmbientOcclusionTexture
+material.aoMapIntensity = 1
+material.displacementMap = doorHeightTexture
+material.displacementScale = 0.05
+material.metalnessMap = doorMetalnessTexture
+material.roughnessMap = doorRoughnessTexture
+material.normalMap = doorNormalTexture
+material.normalScale.set(0.5, 0.5)
+material.alphaMap = doorAlphaTexture
+material.transparent = true // to use alphaMap
+material.side = THREE.DoubleSide
+
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.0001)
+gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
 const pointLight = new THREE.PointLight(0xffffff)
@@ -83,19 +111,31 @@ pointLight.position.z = 4
 scene.add(pointLight)
 
 const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.SphereGeometry(0.5, 64, 64),
   material
 )
 sphere.position.x = - 1.5
+sphere.geometry.setAttribute(
+  'uv2',
+  new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
+)
 
 const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(1, 1),
+  new THREE.PlaneGeometry(1, 1, 100, 100),
   material
+)
+plane.geometry.setAttribute(
+  'uv2',
+  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
 )
 
 const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+  new THREE.TorusGeometry(0.3, 0.2, 64, 128),
   material
+)
+torus.geometry.setAttribute(
+  'uv2',
+  new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2)
 )
 torus.position.x = 1.5
 
