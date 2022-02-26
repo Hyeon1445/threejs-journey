8fb2ede8961828723da7eb623e4b26272a843bc5
Hyeon1445 <lily1445@gmail.com>
2022-02-26 15:15:05 +0900

    15-2. Lights (point light)

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -22,11 +22,12 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01)
 
-// const pointLight = new THREE.PointLight(0xffffff, 0.5)
-// pointLight.position.x = 2
-// pointLight.position.y = 3
-// pointLight.position.z = 4
-// scene.add(pointLight)
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+gui.add(pointLight, 'intensity').min(0).max(1).step(0.01)
 
 /**
  * Objects
