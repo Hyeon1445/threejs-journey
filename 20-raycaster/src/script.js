22b068c4a924965c9436d68f76d6f11855e5a5e0
Hyeon1445 <lily1445@gmail.com>
2022-03-10 23:00:01 +0900

    20-3. raycaster (change ball color with mouse hover event)

--- 20-raycaster/src/script.js
+++ 20-raycaster/src/script.js
@@ -65,6 +65,17 @@ window.addEventListener('resize', () => {
 })
 
 /**
+ * Mouse (Hover)
+ */
+const mouse = new THREE.Vector2() // mouse position (x, y)
+window.addEventListener('mousemove', (_event) => {
+  mouse.x = _event.clientX / sizes.width * 2 - 1
+  mouse.y = - (_event.clientY / sizes.height) * 2 + 1
+  // console.log(mouse)
+})
+
+
+/**
  * Camera
  */
 // Base camera
@@ -99,21 +110,14 @@ const tick = () => {
   object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
   // Cast a ray
-  const rayOrigin = new THREE.Vector3( -3, 0, 0)
-  const rayDirection = new THREE.Vector3(1, 0, 0)
-  rayDirection.normalize()
-  raycaster.set(rayOrigin, rayDirection)
-
+  raycaster.setFromCamera(mouse, camera)
   const objectsToTest = [object1, object2, object3]
   const intersects = raycaster.intersectObjects(objectsToTest)
-  // console.log(intersects.length)
-
   for(const object of objectsToTest) {
-    object.material.color.set('#ff0000') // init objects to red color
+    object.material.color.set('#ff0000')
   }
-
   for(const intersect of intersects) {
-    intersect.object.material.color.set('#0000ff') // when the ball is on the line, set blue color
+    intersect.object.material.color.set('#0000ff')
   }
 
   // Update controls
